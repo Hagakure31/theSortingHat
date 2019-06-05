ee30192bb91669b0d830561b60f4693b106ff2c1
Hagakure31 <luffyeic@gmail.com>
2019-06-05 14:25:10 +0200

    ajout du js

--- js/selectHouse.js
+++ js/selectHouse.js
@@ -1,1 +1,19 @@
-va
+const value = ['400', '401', '403', '404'];
+
+
+function randomize(){
+   return value[(Math.floor(Math.random()*4))];
+}  
+
+
+
+const input = document.getElementById('button');
+const bulle = document.getElementById('bulle')
+const house = document.getElementById('houseNum')
+console.log(bulle);
+input.addEventListener('click', ()=>{
+
+    house.innerHTML= "Vous avez été assigné à la maison:";
+    setTimeout(function(){house.innerHTML = randomize()}, 2000);
+    
+});
