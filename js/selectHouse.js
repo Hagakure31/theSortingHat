3b820dbccd5aaa23681a137d21f88cb083d0fb0b
Jordan <48123323+JordanSOA@users.noreply.github.com>
2019-06-05 16:37:47 +0200

    finalize project

--- js/selectHouse.js
+++ js/selectHouse.js
@@ -1,19 +1,19 @@
 const value = ['400', '401', '403', '404'];
 
-
+// fonction pour attribuer une valeur aléatoire parmis les 4 
 function randomize(){
     return value[(Math.floor(Math.random()*4))];
 }  
 
 
-
 const input = document.getElementById('button');
-const bulle = document.getElementById('bulle')
-const house = document.getElementById('houseNum')
-console.log(bulle);
+const bulle = document.getElementById('bulle');
+const house = document.getElementById('houseNum');
+
+// fonction pour rendre le bouton fonctionnel 
 input.addEventListener('click', ()=>{
 
     house.innerHTML= "Vous avez été assigné à la maison:";
-    setTimeout(function(){house.innerHTML = "                   " +randomize()}, 2000);
+    setTimeout(function(){house.innerHTML = randomize()}, 2000);
     
 });
