43b60afbd4b97201f5300b77d5574015246b506f
Jordan <48123323+JordanSOA@users.noreply.github.com>
2019-06-06 15:07:18 +0200

    remove bulle var

--- js/selectHouse.js
+++ js/selectHouse.js
@@ -1,19 +1,18 @@
 const value = ['400', '401', '403', '404'];
 
 
-function randomize(){
-   return value[(Math.floor(Math.random()*4))];
-}  
+function randomize() {
+    return value[(Math.floor(Math.random() * 4))];
+}
 
 
 
 const input = document.getElementById('button');
-const bulle = document.getElementById('bulle')
 const house = document.getElementById('houseNum')
 console.log(bulle);
-input.addEventListener('click', ()=>{
+input.addEventListener('click', () => {
 
-    house.innerHTML= "Vous avez été assigné à la maison:";
-    setTimeout(function(){house.innerHTML = randomize()}, 2000);
-    
+    house.innerHTML = "Vous avez été assigné à la maison:";
+    setTimeout(function () { house.innerHTML = randomize() }, 2000);
+
 });
